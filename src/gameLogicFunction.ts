61e3a0af9810e37b29e7dd806153731f2fc9fd0f
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-05-19 11:48:52 +0200

    Imported and exported files

--- src/gameLogicFunction.ts
+++ src/gameLogicFunction.ts
@@ -1,1 +1,79 @@
+import { GameStep } from "./types"
+import { gameData } from "./gameData"
 
+const textElement = document.querySelector("#questionText") as HTMLElement 
+const leftBtnElement = document.querySelector("#leftBtn") as HTMLElement 
+const rightBtnElement = document.querySelector("#rightBtn") as HTMLElement
+const startBtnElement = document.querySelector("#startBtn") as HTMLElement
+
+
+// Variabel för array gameData 
+/* let currentState = gameData[0] 
+console.log(currentState) */
+
+/* startDating - function 
+currentStep()*/
+
+
+// Renderar ut frågorna
+export const currentStep: (gameStep: GameStep) => void = (gameStep) => {
+
+  if(!gameStep) {
+    gameStep = gameData[0]
+  }
+ 
+  // Skriver ut options samt eventlyssnare
+  leftBtnElement!.textContent = gameStep.options.left!.text
+  leftBtnElement.addEventListener("click", () => { 
+    nextStep(gameStep.options.left!.path) 
+  })
+
+  rightBtnElement!.textContent = gameStep.options.right!.text // kolla detta
+  rightBtnElement.addEventListener("click", () => { 
+    nextStep(gameStep.options.right!.path)
+  }) 
+
+  // Skriver ut frågan
+  textElement!.textContent = gameStep.question!
+
+}
+
+export const nextStep: (id: number) => void = (id) => { 
+  
+  // forEach
+
+ /*  gameData.forEach((GameStep) => {
+ // ifsats
+    currentStep(nextState) 
+  } */
+
+
+
+  for (let i = 0; i < gameData.length; i++) {
+
+    const nextState = gameData[i];
+      if(nextState.id == id) {
+        currentStep(nextState)       
+      }
+    }
+
+ } 
+
+
+ //Video
+ 
+/*  var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+} */
--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,16 @@
-import { gameData } from './gameData'
-import { GameStep } from './types' 
+import './style.css'
+import { currentStep } from "./gameLogicFunction"
+import { gameData } from './gameData';
 
-const textElement = document.getElementById("questionText") as HTMLElement 
-const leftBtnElement = document.querySelector("#leftBtn") as HTMLElement 
-const rightBtnElement = document.querySelector("#rightBtn") as HTMLElement
+/* import { gameData } from './gameData'
+import { GameStep } from './types' */
+
 
 // Steg 1 - startar spelet
 const startGame:  () => void = () => {
-    currentStep(gameData[0]); 
-  
-  }
-  
-  startGame()
-     
-  
-  // Variabel för array gameData 
-  /* let currentState = gameData[0] 
-  console.log(currentState) */
-  
-  // Renderar ut frågorna
-  export const currentStep: (gameStep: GameStep) => void = (gameStep) => {
-  
-    if(!gameStep) {
-      gameStep = gameData[0]
-    }
-   
-    // Skriver ut options samt eventlyssnare
-    leftBtnElement!.textContent = gameStep.options.left.text
-    leftBtnElement.addEventListener("click", () => { 
-      nextStep(gameStep.options.left.path) 
-    })
-  
-    rightBtnElement!.textContent = gameStep.options.right!.text // kolla detta
-    rightBtnElement.addEventListener("click", () => { 
-      nextStep(gameStep.options.right!.path)
-    }) 
-  
-    // Skriver ut frågan
-    textElement!.textContent = gameStep.question
-  
-  }
-  
-  export const nextStep: (id: number) => void = (id) => { 
-    
-    // forEach
-    /* textNode.options.forEach(option => {
-    if (showOption(option)) { 
-      renderQuestion(nextState) */
+  currentStep(gameData[0]); 
 
-  
-   } 
-  
-  
-  
-  
-  
-  
+}
 
-
-
+startGame()
 
