fc2be46a51bb807123da8f052a011469bb086d58
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-05-26 15:38:09 +0200

    Added functions, fixed the questions, added image

--- src/gameData.ts
+++ src/gameData.ts
@@ -1,63 +1,64 @@
 import { GameStep } from "./types";
 
-// Fixa fler frågor till rätt fråga
 
-export const gameData: GameStep[] = [
+export const gameData: GameStep [] = [
     {
         id: 1,
-        question: 'Är du redo att träffa kärleken?',
+        question: "Är du redo att träffa kärleken?",
         options: 
             {
               left: { 
                     text:"Jag längtar efter kärlek", 
-                    path: 2 
+                    path: 2
                 },
               right: { 
                     text: "Nej jag trivs som ungkarl",
-                    path: 6 
+                    path: 8 
                 }
             }
     }, 
     {
         id: 2,
-        question: 'Jag önskar jag hade någon jag kunde dela ____ med.',
+        question: "Jag önskar jag hade någon jag kunde dela ____ med.",
         options:
             {
               left: { 
-                  text: "Livet",
-                  path: 3
+                  text: "Sängen",
+                  path: 8 
               },
               right: {
-                  text: "Sängen",
-                  path: 1
+                  text: "Spela Boul",
+                  path: 3
             }
         }
     },
     {
         id: 3,
-        question: 'Är du den som dansar på bordet efter några tequlia eller somnar du under bordet?',
+        question: "Är du den som dansar på bordet efter några tequlia eller somnar du under bordet?",
         options: 
             {
-              left: { text: "Dansar alltid på bordet",
-              path: 4
+              left: { 
+                  text: "Dansar alltid på bordet",
+                  path: 4
             },
-              right: { text: "Somnar garanterat under bordet",
-              path: 1
+              right: { 
+                  text: "Somnar garanterat under bordet",
+                  path: 8
             }
         }
     },
     {
         id: 4,
-        question: 'Gillar du att spela boul? ',
+        question: "Vad gör du helst på första Dejten?",
         options:
             {
               left: { 
-                  text: "Det är min favorit sport",
+                  text: "Spelar boul på Boulbar",
                   path: 5
               },
               right: {
-                  text: "Nja, det är för gamlingar",
-                  path: 1
+                  text: "Går på Bio", 
+                  path: 8
             }
         }
     },
@@ -67,23 +68,57 @@ export const gameData: GameStep[] = [
         options: 
             {
               left: {
-                  text: "Pengar, fysisk attraktion, rumpa & bröst ",
-                  path: 1
+                  text: "Pengar, fysisk attraktion, att hen spelar boul ",
+                  path: 8 
               },
               right: {
-                  text: "Trygghet, humor & tillit",
-                  path: 1
+                  text: "Trygghet, humor & att hen spelar boul",
+                  path: 6
             }
         }
     
     },
-    { // Fixa ett steg till gameover
+    {
         id: 6,
-        question: 'Bättre lycka nästa gång!',
+        question: "Vad skulle du ta med dig till en öde ö?",
+        options: 
+            {
+              left: {
+                  text: "Boulspel utan tvekan!",
+                  path: 7
+              },
+              right: {
+                  text: "Givetvis dig!",
+                  path: 8
+            }
+        }
+ 
+    
+    },
+    {
+        id: 7, 
+        image: "../src/images/bild.png", 
+        question: "Wohooo båda gillar Boul!",
+        options: 
+         {
+              left: {
+                  text: "",
+                  path: 1
+              },
+              right: {
+                  text: "",
+                  path: 1
+            }
+        } 
+    
+    },
+    {  
+        id: 8,
+        question: "Vill du ge speed dejting ett nytt försök??",
         options:
             {
               left: {
-                  text: "Vill du börja om?",
+                  text: "Börja om här!",
                   path: 1
             }
     }
